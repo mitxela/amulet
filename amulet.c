5df701d2ae70040d267bca67c3aee6a730823f70
mitxela <mitxela@mitxela.com>
2021-06-09 01:34:16 +0100

    Modes

--- amulet.c
+++ amulet.c
@@ -9,9 +9,13 @@
 // PA4, PA5 - bar graph cathodes
 // PA6      - mode indicator cathode
 
+// PB6      - button
+
 #define MATLEN 24
 
 unsigned char matrix[MATLEN]={0};
+unsigned char mode = 0;
+unsigned char button = 0;
 
 // TIM0_OVF for attiny84
 ISR(TIMER0_OVF_vect) {
@@ -19,6 +23,13 @@ ISR(TIMER0_OVF_vect) {
 
   PORTA = matrix[idx];
   if (++idx == MATLEN) idx = 0;
+}
+
+// EXT_INT0 on attiny84
+ISR(INT0_vect) {
+  GIMSK &= ~(1<<INT0);
+  mode = 0;
+  button = 255;
 }
 
 void set_bar_graph(unsigned char v) {
@@ -52,11 +63,12 @@ int main(void) {
 
   DDRA = 0xFF;
   PORTA = 0xF0;
+  DDRB = 0;
+  PORTB = 1<<6;
 
   TCCR0B = 2; // prescaler /8 - overflow 3906Hz
   TIMSK = (1<<TOIE0);
 
-  set_bar_graph(0);
   sei();
 
 
@@ -69,19 +81,15 @@ int main(void) {
     matrix[1]=0b11011010;
     while (1);
   }
-
-
   i2c_write(0);
   i2c_write(0x10); // power on, int time 100ms
   i2c_stop();
-
-  _delay_ms(101);
+  _delay_ms(10);
 
   while (1) {
-    //unsigned short uva = read_reg(0x07);
-    //unsigned short uvb = read_reg(0x09);
-    //unsigned short uvcomp1 = read_reg(0x0A);
-    //unsigned short uvcomp2 = read_reg(0x0B);
+    _delay_ms(100);
+
+    float uva, uvb, uvcomp1, uvcomp2, uva_calc=0, uvb_calc=0;
 
     #define c_a 2.22
     #define c_b 1.33
@@ -90,26 +98,62 @@ int main(void) {
     #define c_resp_uva 0.001461
     #define c_resp_uvb 0.002591
 
-    //float uva_calc = (float)uva - c_a*(float)uvcomp1 - c_b*(float)uvcomp2;
-    //float uvb_calc = (float)uvb - c_c*(float)uvcomp1 - c_d*(float)uvcomp2;
+    #define UVI_256 23.3
 
 
-    float uva = read_reg(0x07);
-    float uvb = read_reg(0x09);
-    float uvcomp1 = read_reg(0x0A);
-    float uvcomp2 = read_reg(0x0B);
+    while ((PINB & (1<<6)) ==0) { //button held
+      // freeze display
+      _delay_ms(10);
+      button++; if (button==0) button--;
+    }
+    if (button >0) { // button just released
+      if (button <50) { // was held for less than 500ms
+        mode++;
+        if (mode>3) mode=0;
+      }
+      button =0;
+    }
 
-    float uva_calc = uva - c_a*uvcomp1 - c_b*uvcomp2;
-    float uvb_calc = uvb - c_c*uvcomp1 - c_d*uvcomp2;
+    uva = read_reg(0x07);
+    uvb = read_reg(0x09);
+    uvcomp1 = read_reg(0x0A);
+    uvcomp2 = read_reg(0x0B);
+
+    uva_calc = uva - c_a*uvcomp1 - c_b*uvcomp2;
+    uvb_calc = uvb - c_c*uvcomp1 - c_d*uvcomp2;
 
     if (uva_calc<0.0) uva_calc = 0;
     if (uvb_calc<0.0) uvb_calc = 0;
 
-    float uvi = ( uva_calc*c_resp_uva + uvb_calc*c_resp_uvb )*0.5;
 
-    set_bar_graph( (char)( uvi*25 ) );
-    _delay_ms(100);
+    if (mode == 0) {
+      matrix[2]=0b10110010;
+
+      float uvi = ( uva_calc*c_resp_uva + uvb_calc*c_resp_uvb )*0.5*UVI_256;
+      if (uvi >254.0) uvi=254.0;
+      set_bar_graph( (char)( uvi ) );
+
+
+    } else if (mode == 1) {
+      matrix[2]=0b10110100;
+
+      set_bar_graph( (char)( uva_calc * c_resp_uva * UVI_256 ) );
+
+    } else if (mode == 2) {
+      matrix[2]=0b10111000;
+
+      set_bar_graph( (char)( uvb_calc * c_resp_uvb * UVI_256 ) );
+
+    } else {
+      matrix[0]=0b11101100;
+      matrix[1]=0b11011100;
+      matrix[2]=0;
+
+      // MCUCR 0 default for low-level triggered
+      GIMSK |= (1<<INT0);
+      MCUCR |= (1<<SE) | (1<<SM1);
+      asm("sleep");
+    }
+
   }
-
-
 }
